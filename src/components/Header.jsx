449afa95c85265b646f57460030cdcec8a51c05d
jhoni <yohannesjho12@gmail.com>
2022-11-23 22:30:14 +0300

    complete the header component and make dyanamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,67 @@
-import React from 'react'
-import { useLocation , useNavigate} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 export default function Header() {
-    const location = useLocation();
-    const navigate=useNavigate();
-    function pathMatchRoute(route) {
-        if (route === location.pathname) {
-          return true;
-        }
-      }
-  return (
-    <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
-       <header className='flex justify-between    px-4 max-w-6xl mx-auto items-center'>
-          <div >
-            <img onClick={()=>navigate("/")} className="h-5 cursor-pointer " src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" />
-          </div>
+  const location = useLocation();
+  const navigate = useNavigate();
+  const auth = getAuth();
+  function pathMatchRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
 
-          <div >
-            <ul className='flex space-x-10 '>
+  const [pageState, setPageState] = useState("sign-in");
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("profile");
+      } else setPageState("sign-in");
+    });
+  }, [auth]);
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className="flex justify-between    px-4 max-w-6xl mx-auto items-center">
+        <div>
+          <img
+            onClick={() => navigate("/")}
+            className="h-5 cursor-pointer "
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="logo"
+          />
+        </div>
+
+        <div>
+          <ul className="flex space-x-10 ">
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
                 pathMatchRoute("/") && "text-black border-b-red-500"
               }`}
-               onClick={()=>navigate("/")}
+              onClick={() => navigate("/")}
             >
               Home
             </li>
-                <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
                 pathMatchRoute("/offers") && "text-black border-b-red-500"
-              }`} 
-              onClick={()=>navigate("/offers")}>
-                Offers</li>
-                <li  className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/sign-in") && "text-black border-b-red-500"
-              }`} onClick={()=>navigate("/sign-in")}
-              >
-                SignIn</li>
-            </ul>
-          </div>
-       </header>
+              }`}
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
+                "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/profile")}
+            >
+              {pageState}
+            </li>
+          </ul>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
