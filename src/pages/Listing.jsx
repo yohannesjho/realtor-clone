7dc5492b63b1450c3e3783b0705ffc401731c954
jhoni <yohannesjho12@gmail.com>
2022-12-04 02:43:41 +0300

    add map to the listing page using leaflet and react-leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -16,11 +16,12 @@ import SwiperCore, {
 } from "swiper";
 import "swiper/css/bundle";
 import Contact from "../components/Contact";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Listing() {
   const auth = getAuth();
   const params = useParams();
-  const [contactLandlord,setContactLandlord] = useState(false)
+  const [contactLandlord, setContactLandlord] = useState(false);
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   const [shareLinkCopied, setShareLinkCopied] = useState(false);
@@ -137,7 +138,7 @@ export default function Listing() {
             </li>
           </ul>
           {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
-            <div onClick={()=>setContactLandlord(true)} className="mt-6">
+            <div onClick={() => setContactLandlord(true)} className="mt-6">
               <button className="bg-blue-600 py-3 px-7 text-white text-center uppercase font-medium rounded shadow-md hover:shadow-lg hover:bg-blue-700 focus:bg-blue-700 focus:shadow-lg transition duration-150 ease-in-out w-full ">
                 contact the landlord
               </button>
@@ -148,7 +149,21 @@ export default function Listing() {
             <Contact userRef={listing.userRef} listing={listing} />
           )}
         </div>
-        <div className="bg-blue-300 w-full h-[200px] lg:h-[400px] "></div>
+        <div className="  w-full h-[200px] lg:h-[400px] ">
+          <MapContainer center={[listing.geolocation.lat, listing.geolocation.lng]} zoom={13} scrollWheelZoom={false}
+          style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </div>
     </main>
   );
